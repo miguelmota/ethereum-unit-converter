55f544c21a4b1a0966bd8fe0cbd313c29c2a099c
Miguel Mota <hello@miguelmota.com>
2021-06-04 15:31:42 -0700

    Add linter

--- example/dist/bundle.js
+++ example/dist/bundle.js
@@ -4,11 +4,11 @@
 var BN = require('bignumber.js');
 
 function convertToEther(value, unit) {
-  if (value == undefined) {
+  if (value === undefined) {
     throw TypeError('value is required');
   }
 
-  if (unit == undefined) {
+  if (unit === undefined) {
     throw TypeError('unit is required');
   }
 
@@ -53,17 +53,39 @@ function converter(value, unit, toUnit) {
 
   result[unit] = new BN(value).toString(10);
 
-  if (unit !== 'wei') result['wei'] = v.times(new BN(1000000000000000000)).toString(10);
-  if (unit !== 'kwei') result['kwei'] = v.times(new BN(1000000000000000)).toString(10);
-  if (unit !== 'mwei') result['mwei'] = v.times(new BN(1000000000000)).toString(10);
-  if (unit !== 'gwei') result['gwei'] = v.times(new BN(1000000000)).toString(10);
-  if (unit != 'szabo') result['szabo'] = v.times(new BN(1000000)).toString(10);
-  if (unit != 'finney') result['finney'] = v.times(new BN(1000)).toString(10);
-  if (unit != 'ether') result['ether'] = v.times(new BN(1)).toString(10);
-  if (unit !== 'kether') result['kether'] = v.times(new BN(0.001)).toString(10);
-  if (unit !== 'mether') result['mether'] = v.times(new BN(0.000001)).toString(10);
-  if (unit !== 'gether') result['gether'] = v.times(new BN(0.000000001)).toString(10);
-  if (unit !== 'tether') result['tether'] = v.times(new BN(0.000000000001)).toString(10);
+  if (unit !== 'wei') {
+    result.wei = v.times(new BN(1000000000000000000)).toString(10);
+  }
+  if (unit !== 'kwei') {
+    result.kwei = v.times(new BN(1000000000000000)).toString(10);
+  }
+  if (unit !== 'mwei') {
+    result.mwei = v.times(new BN(1000000000000)).toString(10);
+  }
+  if (unit !== 'gwei') {
+    result.gwei = v.times(new BN(1000000000)).toString(10);
+  }
+  if (unit !== 'szabo') {
+    result.szabo = v.times(new BN(1000000)).toString(10);
+  }
+  if (unit !== 'finney') {
+    result.finney = v.times(new BN(1000)).toString(10);
+  }
+  if (unit !== 'ether') {
+    result.ether = v.times(new BN(1)).toString(10);
+  }
+  if (unit !== 'kether') {
+    result.kether = v.times(new BN(0.001)).toString(10);
+  }
+  if (unit !== 'mether') {
+    result.mether = v.times(new BN(0.000001)).toString(10);
+  }
+  if (unit !== 'gether') {
+    result.gether = v.times(new BN(0.000000001)).toString(10);
+  }
+  if (unit !== 'tether') {
+    result.tether = v.times(new BN(0.000000000001)).toString(10);
+  }
 
   if (toUnit) {
     toUnit = toUnit.trim().toLowerCase();
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const BN = require('bignumber.js')
 
-function convertToEther(value, unit) {
-  if (value == undefined) {
+function convertToEther (value, unit) {
+  if (value === undefined) {
     throw TypeError('value is required')
   }
 
-  if (unit == undefined) {
+  if (unit === undefined) {
     throw TypeError('unit is required')
   }
 
@@ -13,13 +13,13 @@ function convertToEther(value, unit) {
   unit = unit.toLowerCase()
   if (unit === 'eth') unit = 'ether'
 
-  if (unit === 'wei'   ) return v.times(new BN(0.000000000000000001))
-  if (unit === 'kwei'  ) return v.times(new BN(0.000000000000001))
-  if (unit === 'mwei'  ) return v.times(new BN(0.000000000001))
-  if (unit === 'gwei'  ) return v.times(new BN(0.000000001))
-  if (unit === 'szabo' ) return v.times(new BN(0.000001))
+  if (unit === 'wei') return v.times(new BN(0.000000000000000001))
+  if (unit === 'kwei') return v.times(new BN(0.000000000000001))
+  if (unit === 'mwei') return v.times(new BN(0.000000000001))
+  if (unit === 'gwei') return v.times(new BN(0.000000001))
+  if (unit === 'szabo') return v.times(new BN(0.000001))
   if (unit === 'finney') return v.times(new BN(0.001))
-  if (unit === 'ether' ) return v.times(new BN(1))
+  if (unit === 'ether') return v.times(new BN(1))
   if (unit === 'kether') return v.times(new BN(1000))
   if (unit === 'mether') return v.times(new BN(1000000))
   if (unit === 'gether') return v.times(new BN(1000000000))
@@ -28,7 +28,7 @@ function convertToEther(value, unit) {
   throw TypeError('Invalid unit')
 }
 
-function converter(value, unit, toUnit) {
+function converter (value, unit, toUnit) {
   const v = convertToEther(value, unit)
   unit = unit.toLowerCase()
   if (unit === 'eth') unit = 'ether'
@@ -50,28 +50,17 @@ function converter(value, unit, toUnit) {
 
   result[unit] = new BN(value).toString(10)
 
-  if (unit !== 'wei')
-    result['wei']    = v.times(new BN(1000000000000000000)).toString(10)
-  if (unit !== 'kwei')
-    result['kwei']   = v.times(new BN(1000000000000000)).toString(10)
-  if (unit !== 'mwei')
-    result['mwei']   = v.times(new BN(1000000000000)).toString(10)
-  if (unit !== 'gwei')
-    result['gwei']   = v.times(new BN(1000000000)).toString(10)
-  if (unit != 'szabo')
-    result['szabo']  = v.times(new BN(1000000)).toString(10)
-  if (unit != 'finney')
-    result['finney'] = v.times(new BN(1000)).toString(10)
-  if (unit != 'ether')
-    result['ether']  = v.times(new BN(1)).toString(10)
-  if (unit !== 'kether')
-    result['kether'] = v.times(new BN(0.001)).toString(10)
-  if (unit !== 'mether')
-    result['mether'] = v.times(new BN(0.000001)).toString(10)
-  if (unit !== 'gether')
-    result['gether'] = v.times(new BN(0.000000001)).toString(10)
-  if (unit !== 'tether')
-    result['tether'] = v.times(new BN(0.000000000001)).toString(10)
+  if (unit !== 'wei') { result.wei = v.times(new BN(1000000000000000000)).toString(10) }
+  if (unit !== 'kwei') { result.kwei = v.times(new BN(1000000000000000)).toString(10) }
+  if (unit !== 'mwei') { result.mwei = v.times(new BN(1000000000000)).toString(10) }
+  if (unit !== 'gwei') { result.gwei = v.times(new BN(1000000000)).toString(10) }
+  if (unit !== 'szabo') { result.szabo = v.times(new BN(1000000)).toString(10) }
+  if (unit !== 'finney') { result.finney = v.times(new BN(1000)).toString(10) }
+  if (unit !== 'ether') { result.ether = v.times(new BN(1)).toString(10) }
+  if (unit !== 'kether') { result.kether = v.times(new BN(0.001)).toString(10) }
+  if (unit !== 'mether') { result.mether = v.times(new BN(0.000001)).toString(10) }
+  if (unit !== 'gether') { result.gether = v.times(new BN(0.000000001)).toString(10) }
+  if (unit !== 'tether') { result.tether = v.times(new BN(0.000000000001)).toString(10) }
 
   if (toUnit) {
     toUnit = toUnit.trim().toLowerCase()
